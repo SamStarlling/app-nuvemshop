4352a8109dba5b9d77b858a4407dd48efcf1f2ba
SamStarlling <samelabarbosa.06@gmail.com>
2022-07-07 21:31:46 -0300

    feat: Add two routes one for get all products ids and other to get all variants for specific product id

--- src/modules/products/products.controller.ts
+++ src/modules/products/products.controller.ts
@@ -1,17 +1,23 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { query } from 'express';
 import { ProductsService } from './products.service';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
-
   @Get()
-  findAll() {
-    return this.productsService.findAll();
+  async findAllProductsId(@Query('user_id') user_id: string) {
+    return await this.productsService.getProductsID(parseInt(user_id));
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.productsService.findOne(+id);
+  @Get('variants')
+  async findProductIdVariants(
+    @Query('user_id') user_id: string,
+    @Query('product_id') product_id: string,
+  ) {
+    return await this.productsService.getAllProductsVariants(
+      parseInt(user_id),
+      parseInt(product_id),
+    );
   }
 }
--- src/modules/products/products.service.ts
+++ src/modules/products/products.service.ts
@@ -1,12 +1,78 @@
-import { Injectable } from '@nestjs/common';
+import { lastValueFrom } from 'rxjs';
+import { HttpService } from '@nestjs/axios';
+import { CredentialsEntity } from './../entities/credentials.entity';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ProductsService {
-  findAll() {
-    return `This action returns all products`;
+  constructor(
+    private readonly httpService: HttpService,
+    @InjectRepository(CredentialsEntity)
+    private readonly credentialsRepository: Repository<CredentialsEntity>,
+  ) {}
+
+  getCredentials(user: object) {
+    const { user_id, token } = user[0];
+    return { user_id, token };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async getHttpRequest(url: string, token: string) {
+    return await lastValueFrom(
+      this.httpService.get(url, {
+        headers: {
+          'Content-Type': 'application/json',
+          'User-Agent': 'Awesome App {samela.silva@nuvemshop.com.br}',
+          Authentication: `bearer ${token}`,
+        },
+      }),
+    );
   }
+  async getProductsID(userId: number) {
+    const user = await this.credentialsRepository.find(userId);
+    const { user_id, token } = this.getCredentials(user);
+
+    const url = `https://api.nuvemshop.com.br/v1/${user_id}/products?fields=id`;
+    if (!user) {
+      throw new BadRequestException('User not found!');
+    }
+    const { data } = await this.getHttpRequest(url, token);
+    return {
+      data: data, //This is array of products ID
+      url: `https://api.nuvemshop.com.br/v1/${user_id}/products`,
+      user: token,
+    };
+    // return data;
+  }
+  async getAllProductsVariants(user_id: number, product_id: number) {
+    const { url, user: token } = await this.getProductsID(user_id);
+    // const productsVariant = [];
+    // const fields = [
+    //   'id',
+    //   'product_id',
+    //   'name',
+    //   'price',
+    //   'promotional_price',
+    //   'stock',
+    // ];
+    const urlGetAllVariants = `${url}/${product_id}/variants`;
+    // ?fields=${fields.join(',')}`;
+    const { data } = await this.getHttpRequest(urlGetAllVariants, token);
+    return data;
+  }
+  // getProductVariantsID(products: []): Array<IProductVariantsID> {
+  //   return products.map((productArray: []) => {
+  //     for (let index = 0; index < productArray.length; index += 1) {
+  //       const { stock, id } = productArray[index];
+  //       if (stock < 10) {
+  //         return id;
+  //       }
+  //     }
+  //   });
+  // }
+  // async changePriceOfProducts(product_id) {}
+  // findOne(id: number) {
+  //   return `This action returns a #${id} product`;
+  // }
 }
