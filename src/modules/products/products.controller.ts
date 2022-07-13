078ea51dfa44e7f0e68c2eef4f3c64ae8e173907
SamStarlling <samelabarbosa.06@gmail.com>
2022-07-13 17:02:58 -0300

    feat: Add new route /variants to get all variants of product

--- src/modules/products/products.controller.ts
+++ src/modules/products/products.controller.ts
@@ -4,19 +4,28 @@ import { ProductsService } from './products.service';
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
-  @Get()
-  async findAllProductsId(@Query('user_id') user_id: string) {
-    return await this.productsService.getProductsID(parseInt(user_id));
-  }
 
-  @Get('variants')
-  async findProductIdVariants(
+  @Get()
+  async findProductById(
     @Query('user_id') user_id: string,
     @Query('product_id') product_id: string,
   ) {
-    return await this.productsService.getAllProductsVariants(
+    return await this.productsService.getProductById(
       parseInt(user_id),
       parseInt(product_id),
+    );
+  }
+
+  @Get('variants')
+  async findAllVariantsOfProductOrdered(
+    @Query('user_id') user_id: string,
+    @Query('product_id') product_id: string,
+    @Query('order') order: string,
+  ) {
+    return await this.productsService.getOrderProductByDiscount(
+      parseInt(user_id),
+      parseInt(product_id),
+      order,
     );
   }
 }
